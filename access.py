544e57a6ab1d8fa781407349ad053d01a1fd67f0
Siddhartha Sapkota <siddharthasapkota03@gmail.com>
2025-07-05 08:11:12 +0545

    Simple if and while loop on ATBR

---
+++ access.py
@@ -0,0 +1,10 @@
+name = "Mary"
+password = "password"
+
+if name == "Mary":
+    print("Hello, Mary")
+    if password == "password":
+        print("Access Granted.")
+    else:
+        print("Wrong Password")
+
---
+++ name.py
@@ -0,0 +1,19 @@
+# Simple Naming Code
+
+name = 'Alice'
+age = 12
+'''
+
+if name == 'Alice':
+    print('Hi, Alice.')
+else:
+    print('Hi Stranger!')
+'''
+# name = input("What's your name? ")
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, Kiddo.')
+else:
+    print('End')
+
